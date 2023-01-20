ce104888e7320ea6cc0a154828a881a79ca45ea0
Kate Yasinskaya <katherine_yasinskaya@mail.ru>
2023-01-20 17:55:18 +0300

    add separate stylesheets

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 
+ let section = document.querySelectorAll("section");
+ let menu = document.querySelectorAll("header nav ul li a");
+ window.onscroll = () => {
+   section.forEach((i) => {
+     let top = window.scrollY;
+     let offset = i.offsetTop - 150;
+     let height = i.offsetHeight;
+     let id = i.getAttribute("id");
+     if (top >= offset && top < offset + height) {
+       menu.forEach((link) => {
+         link.classList.remove("active");
+         document.querySelector("header nav ul li a[href*=" + id + "]")
+           .classList.add("active");
+       });
+     }
+   });
+ };
+
  function reveal() {
     var reveals = document.querySelectorAll(".reveal");
   
@@ -17,20 +35,6 @@
   
   window.addEventListener("scroll", reveal);
 
-  let section = document.querySelectorAll('section');
-  let menu = document.querySelectorAll('header nav ul li a');
-  window.onscroll = () => {
-    section.forEach(i => {
-      let top = window.scrollY;
-      let offset = i.offsetTop - 150;
-      let height = i.offsetHeight;
-      let id = i.getAttribute('id');
-      if (top >= offset && top < offset + height) {
-        menu.forEach(link => {
-          link.classList.remove('active');
-          document.querySelector('header nav ul li a[href*=' + id + ']')
-            .classList.add('active');
-        });
-      }
-    });
-  };
+  reveal();
+
+  
